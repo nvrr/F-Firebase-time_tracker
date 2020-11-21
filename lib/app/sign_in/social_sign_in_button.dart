a9c955f20458032921027e2200d3c215ecd69890
nvrr <sureshvarma809@gmail.com>
2020-11-21 18:33:05 +0530

    Configuring Firebase got Android and installing firebase_core and firebase_auth

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    @required String assetName,
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  })  : assert(text != null),
+        assert(assetName != null),
+        super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(assetName),
+              Text(text, style: TextStyle(color: textColor, fontSize: 15.0)),
+              Opacity(
+                opacity: 0.0,
+                child: Image.asset(assetName),
+              ),
+            ],
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
