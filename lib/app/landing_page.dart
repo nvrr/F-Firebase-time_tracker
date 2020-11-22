8eb911fb395d44db93148b098d9748dbe3dc43fe
nvrr <sureshvarma809@gmail.com>
2020-11-22 10:17:06 +0530

    Creating a landing page widget

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'sign_in/sign_in_page.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SignInPage();
+  }
+}
