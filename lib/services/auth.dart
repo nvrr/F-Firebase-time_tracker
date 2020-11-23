fd54aadf3a5fcf20e928c6bd783553d742010631
nvrr <sureshvarma809@gmail.com>
2020-11-23 10:07:06 +0530

    THe Abstract AuthBase class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+abstract class AuthBase {
+  User get currentUser;
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
+
 class Auth {
 
   final _firebaseAuth = FirebaseAuth.instance;
