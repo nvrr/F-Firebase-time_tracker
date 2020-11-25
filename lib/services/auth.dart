aaa931caa236ffb8a87c32cfbfda02adfc07fa93
nvrr <sureshvarma809@gmail.com>
2020-11-25 07:14:12 +0530

    Adding Google Sign-In to the Auth class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User get currentUser;
   Stream<User> authStateChanges();
   Future<User> signInAnonymously();
+  Future<User> signInGoogle();
   Future<void> signOut();
 }
 
@@ -11,15 +13,46 @@ class Auth implements AuthBase{
 
   final _firebaseAuth = FirebaseAuth.instance;
 
+  @override
   Stream<User> authStateChanges() => _firebaseAuth.authStateChanges();
-
+ 
+  @override
   User get currentUser => _firebaseAuth.currentUser;
 
+  @override
   Future<User> signInAnonymously() async {
     final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
   }
+  
+  @override
+  Future<User> signInGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+          ));
+          return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
+          message: 'Missing Google ID Token'
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
 
+  @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
