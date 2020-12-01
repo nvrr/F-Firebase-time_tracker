319be378ceac01dbb20732adb24a8a162b7e3437
nvrr <sureshvarma809@gmail.com>
2020-12-01 16:30:10 +0530

    Toggling the form type

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/common_widgets/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
 
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
   void _submit() {
-    print('email: ${_emailController.text}, password: ${_passwordController.text}');
+    print(
+        'email: ${_emailController.text}, password: ${_passwordController.text}');
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
   }
 
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an accout';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
     return [
       TextField(
         controller: _emailController,
@@ -29,16 +54,17 @@ class EmailSignInForm extends StatelessWidget {
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        text: 'Sign in',
+        text: primaryText,
         onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       FlatButton(
-        child: Text('Need an account? Register'),
-        onPressed: () {},
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
       ),
     ];
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
