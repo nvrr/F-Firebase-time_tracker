4ba88d306bddca1d76694b0e3a373761dfa423be
nvrr <sureshvarma809@gmail.com>
2020-12-06 18:12:05 +0530

    Adding an email and password validation mixin

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/validators.dart';
 import 'package:time_tracker/common_widgets/form_submit_button.dart';
 import 'package:time_tracker/services/auth.dart';
 
 enum EmailSignInFormType { signIn, register }
 
-class EmailSignInForm extends StatefulWidget {
+class EmailSignInForm extends StatefulWidget with EmailAndPasswordValidators{
   final AuthBase auth;
   EmailSignInForm({@required this.auth});
 
@@ -57,7 +58,8 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         ? 'Need an account? Register'
         : 'Have an account? Sign in';
 
-    bool submitEnabled = _email.isNotEmpty && _password.isNotEmpty;
+    bool submitEnabled = widget.emailValidator.isValid(_email) &&
+     widget.passwordValidator.isValid(_email);
 
     return [
       _buildEmailTextField(),
